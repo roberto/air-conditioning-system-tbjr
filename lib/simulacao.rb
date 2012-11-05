4683173072b6c45ee628b60f1152bbffee273f1c
Roberto Soares <roberto.tech@gmail.com>
2012-11-05 12:00:05 -0300

    simulacao class

--- lib/simulacao.rb
+++ lib/simulacao.rb
@@ -1,19 +1,19 @@
-#!/usr/bin/env ruby
+class Simulacao
+  def self.executa
+    temperatura_inicial = 30
+    temperatura_ideal = 20
+    minutos = 360
+    variacao = 0.5
 
-require_relative '../lib/acme'
+    ar_condicionado = ACME::ArCondicionado.new(temperatura_inicial, temperatura_ideal)
 
-temperatura_inicial = 30
-temperatura_ideal = 20
-minutos = 360
-variacao = 0.5
+    ar_condicionado.reajusta
+    minutos.times do
+      ar_condicionado.aumenta_temperatura(variacao)
+      ar_condicionado.reajusta
+    end
 
-ar_condicionado = ACME::ArCondicionado.new(temperatura_inicial, temperatura_ideal)
-
-ar_condicionado.reajusta
-minutos.times do
-  ar_condicionado.aumenta_temperatura(variacao)
-  ar_condicionado.reajusta
+    puts "Temperatura: #{ar_condicionado.temperatura.round(2)}"
+    puts "Custo total: #{ar_condicionado.custo_total.round(2)}"
+  end
 end
-
-puts "Temperatura: #{ar_condicionado.temperatura.round(2)}"
-puts "Custo total: #{ar_condicionado.custo_total.round(2)}"
